e6ea1d0f074fa4713e700fd54897fb4d6b9654a7
Len Payne <len.payne@lambtoncollege.ca>
2015-01-20 13:51:12 -0500

    Initial Commit

---
+++ gen/com/example/helloandroid_cpd4414_02/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.helloandroid_cpd4414_02;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
